c74d5f39a6b5d2fd6c8d6dc4f7a3bc2787e1801c
Kenneth Ventress <jrventress20@gmail.com>
2023-07-12 23:37:01 +0000

    retry push to main

--- app.py
+++ app.py
@@ -13,32 +13,54 @@ def home():
     """ Home Page """
     return render_template('home.html')
 
-@app.route('/search', methods=['GET', 'POST'])
-def search():
+@app.route('/search', methods=['GET', 'POST']) #look up if GET is needed
+def search(): #works here
+    print("SEARCH ROUTE")
     if request.method == 'POST':
-        search_query = request.form.get('search_query')
+        print("This is a POST request")
+        print(request.form)
+        search_query = request.form.get('drink_name')
+        print(f'We have a search query: {search_query}')
+        print(" ")
+        print(" ")
+        #print(type(search_query))
         if search_query:
-            #print(search_query)
             # Perform search based on search_query
-            search_query = get_every_drink_data(search_query)
-            return redirect(url_for('search_results', search_query=search_query))
-    return render_template('search.html')
+            #list_all_drink_info = get_every_drink_data(search_query)
+            #print(f'this is my list of all drink info: {list_all_drink_info }')
+            return redirect(url_for('search_results', search_query=search_query ))
+    return render_template('search.html') #search template but edited to have "no search results found"
 
-@app.route('/search_results/')
-def search_results():
-    search_query = request.args.get('search_query')
-    search_results = get_every_drink_data(search_query)
-    return render_template('search_results.html', search_results=search_results)
+@app.route('/search_results/<search_query>', methods=['GET', 'POST'])
+def search_results(search_query):
+    print(f'Search_results route works')
+    print(" ")
+    print(" ")
+    print(" ")
+    drink_list = get_every_drink_data(search_query)
+    
+    print(f'This is drink list: {drink_list}')
+    return render_template('search_results.html', drink_list=drink_list)
 
-@app.route('/search_results/drink_info/<recipe_dict>')
-def drink_info(recipe_dict):
-    return render_template('drink_info.html', recipe_dict=recipe_dict)
+@app.route('/search_results/drink_info/<recipe_name>')
+def drink_info(recipe_name):
+    # pass the drink name in here into rout and then take it
+    # call a function that gives all the info for that particular drink
+    # return it to drink_info page
+
+    print(f'We want this EXACT DRINK: {recipe_name}')
+    drink_list = get_every_drink_data(recipe_name)
+    our_drink = drink_list[0]
+    print(our_drink)
+    
+    #recipe_dict = request.args.get('recipe_dict')
+
+    return render_template('drink_info.html', recipe_dict=our_drink)
 
 @app.route('/about')
 def about():
     """ about page linked """
     return render_template('about.html')
-
 
 # make own database and add it to API's as another option...
 @app.route('/contact', methods=['GET', 'POST'])
