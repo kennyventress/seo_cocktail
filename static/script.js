60cf7b2a85b51aa4ae509a77d88203931c1bf0ec
Kenneth Ventress <jrventress20@gmail.com>
2023-07-14 01:14:11 +0000

    navbar (almost) finished and set hehe

--- static/script.js
+++ static/script.js
@@ -1,6 +1,23 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('.list-group-item');
 
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
+    links.forEach(function(link) {
+      link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const route = this.getAttribute('data-route');
+        const url = "{{ url_for('') }}" + route;  // Replace '' with the appropriate Flask endpoint
+
+        // Perform any desired actions or navigation with the generated URL
+        // For example, you can use window.location.href to navigate to the URL
+        window.location.href = url;
+      });
+    });
+  });
+</script>
+
+
+{
+  /* The following line can be included in your src/index.js or App.js file */
+}
+import 'bootstrap/dist/css/bootstrap.min.css';
