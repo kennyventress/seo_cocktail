ce9a64ea79786bba8e005f7bde248cb234a94bf9
Abem Lucas <abemlucas12@gmail.com>
2023-07-14 02:44:20 +0000

    added the contact

---
+++ form.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, RadioField, SubmitField, StringField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class ReviewForm(FlaskForm):
+    review_text = TextAreaField('Cocktail Feedbacks!', validators=[DataRequired()])
+    rating = RadioField('Feedback', validators=[DataRequired()], choices=[(1, '1 Star'), (2, '2 Stars'), (3, '3 Stars'), (4, '4 Stars'), (5,'5 Stars')])
+    submit = SubmitField('Submit Response!')
